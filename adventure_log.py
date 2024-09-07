b6f649fccbc2494f5f4975eff80f230ba5c95db9
tomautry <tomuhtree@gmail.com>
2024-09-07 18:07:59 -0500

    Updated Python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Ahoy, Coding Temple Navigators! Ready for today's adventure?")
+    print("Hello, Coding Temple Navigators!")
 
 if __name__ == "__main__":
     greet_coding_temple()
